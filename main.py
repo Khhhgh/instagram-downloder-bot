443aa1ffb681046116693b2b8b2d4e0ae0e3edd9
Khhhgh <103472630+Khhhgh@users.noreply.github.com>
2025-08-19 17:07:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from aiogram import Bot, Dispatcher, executor, types
 import logging
 from downloader import InstagramVideoDownloader
 import shutil
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
+# ضع التوكن مباشرة هنا
+BOT_TOKEN = "7610698647:AAGc0vKL1iYTchSGJowB7FzlLWhwrPTg3V4"
 
-bot = "7610698647:AAGc0vKL1iYTchSGJowB7FzlLWhwrPTg3V4"
+bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
@@ -16,15 +16,17 @@ async def start(message: types.Message):
 
 @dp.message_handler()
 async def mainpart(message: types.Message):
-    global downloaded
     await message.answer("Video yuklanmoqda...")
-    if message.text.startswith("https://www.instagram.com/") or message.text.startswith("https://instagram.com/"):
-        downloaded = InstagramVideoDownloader(message.text)
-        with open(downloaded[1][0], 'rb') as video:
-                with open(downloaded[0], "rb") as comment:
-                    await message.answer_video(video, caption=comment.read().decode("utf-8"))
-    shutil.rmtree(downloaded[2])
-
+    try:
+        if message.text.startswith("https://www.instagram.com/") or message.text.startswith("https://instagram.com/"):
+            txt_file, video_file, folder = InstagramVideoDownloader(message.text)
+            with open(video_file, 'rb') as video, open(txt_file, 'r', encoding='utf-8') as comment:
+                await message.answer_video(video, caption=comment.read())
+    except Exception as e:
+        await message.answer(f"❌ Xatolik yuz berdi: {e}")
+    finally:
+        if 'folder' in locals() and os.path.exists(folder):
+            shutil.rmtree(folder)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
